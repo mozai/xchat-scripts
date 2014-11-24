b7adda8a4e2c97d98637707c54d26b0a1dbfd198
Moses Moore <moses-github@mozai.com>
2014-11-23 22:44:32 -0500

    syntactic sugar for pep8

--- clementine_xchat.py
+++ clementine_xchat.py
@@ -4,127 +4,139 @@ from __future__ import print_function
 import xchat
 import dbus
 
-__module_name__  = "Clementine"
-__module_version__  = "20130620"
-__module_description__  = "control music from inside xchat"
-__module_author__  =  "Mozai"
+__module_name__ = "Clementine"
+__module_version__ = "20131201"
+__module_description__ = "control music from inside xchat"
+__module_author__ = "Mozai"
 
 
 # MPRIS GetStatus() = [ [Playing, Paused, Stopped],
-#                       [Sequential, Shuffle], 
+#                       [Sequential, Shuffle],
 #                       [Play Song Once, Repeat Song],
 #                       [Play List Once, Repeat List]
 #                     ]
 
 def _clementine_iface():
   try:
-    BUS = dbus.SessionBus() 
+    thebus = dbus.SessionBus()
     # I tried leaving BUS as a module global, but it stopped working
     # do dbus sessions time out?
-    playr = BUS.get_object('org.mpris.clementine', '/Player')
+    playr = thebus.get_object('org.mpris.clementine', '/Player')
     iface = dbus.Interface(playr, dbus_interface='org.freedesktop.MediaPlayer')
     return iface
-  except dbus.exceptions.DBusException :
+  except dbus.exceptions.DBusException:
     # "org.freedesktop.DBus.Error.ServiceUnknown" Clementine isn't running
     return None
-  
+
+
 def clem_np(iface):
   songdata = iface.GetMetadata()
   tna = [songdata[i] for i in ('title', 'artist') if songdata.get(i)]
   loc = songdata.get('location')
   if tna:
-    tna = " - ".join(tna)
-    tna = tna[:255] # sanity check
-    xchat.command("me is listening to \x02%s\x02" % tna.encode('utf-8') )
+    tna = "\x02 by \x02".join(tna)
+    tna = "\x02" + tna[:255] + "\x02"  # sanity check
+    xchat.command("me is listening to %s" % tna.encode('utf-8'))
   elif loc:
-    if(loc.startswith('file:')):
+    if loc.startswith('file:'):
       # I want not the full path
-      loc = loc[loc.rfind('/', 0, loc.rfind('/'))+1:]
-      loc = loc[:255] # sanity check
+      loc = loc[loc.rfind('/', 0, loc.rfind('/')) + 1:]
+      loc = loc[:255]  # sanity check
     xchat.command("me is listening to \x02%s\x02" % loc.encode('utf-8'))
   else:
     print("clementine isn't playing")
   return xchat.EAT_NONE
 
+
 def clem_play(iface):
   stat = iface.GetStatus()[0]
-  if(stat == 0):
+  if stat == 0:
     print("clementine is already playing")
   else:
     print("clementine starts playing")
     iface.Play()
-  
+
+
 def clem_stop(iface):
   stat = iface.GetStatus()[0]
-  if(stat == 2):
+  if stat == 2:
     print("clementine is already stopped")
   else:
     print("clementine stops")
     iface.Stop()
 
+
 def clem_pause(iface):
-  iface.Pause()    
+  iface.Pause()
+
 
 def clem_next(iface):
   iface.Next()
-    
+
+
 def clem_prev(iface):
   iface.Prev()
 
+
 def clem_quit(iface):
-  del(iface) # shut up pylint
+  del(iface)  # shut up pylint
   print("killing clementine")
-  BUS.get_object('org.mpris.clementine', '/').Quit()
+  thebus = dbus.SessionBus()
+  thebus.get_object('org.mpris.clementine', '/').Quit()
+
 
 def clem_info(iface):
   mdata = iface.GetMetadata()
   if mdata:
     blurb = ''
-    for i in sorted(mdata.keys()): 
-      blurb += "\x02%s\x02 %s " % (str(i), str(mdata[i]))
+    for i in sorted(mdata.keys()):
+      blurb += "\x02%s\x02 %s " % (i, mdata[i])
     print(blurb.encode('utf-8'))
   else:
     print("clementine not playing?")
 
+
 def clem_help(iface):
-  del(iface) # shut up pylint
+  del(iface)  # shut up pylint
   print('\x02/clem\x02 [ ' + ', '.join(PARAMS.keys()) + ' ]')
-  
-PARAMS = { 
-           'np':    clem_np,
-           'play':  clem_play,
-           'stop':  clem_stop,
-           'pause': clem_pause,
-           'next':  clem_next,
-           'prev':  clem_prev,
-           'info':  clem_info,
-           'quit':  clem_quit,
-           'help':  clem_help,
-        }
+
+PARAMS = {
+  'np':    clem_np,
+  'play':  clem_play,
+  'stop':  clem_stop,
+  'pause': clem_pause,
+  'next':  clem_next,
+  'prev':  clem_prev,
+  'info':  clem_info,
+  'quit':  clem_quit,
+  'help':  clem_help,
+}
+
 
 def clem(word, word_eol, userdata):
   " dispatcher for the commands in PARAMS dict() "
-  del(word_eol, userdata) # shut up, pylint
+  del(word_eol, userdata)  # shut up, pylint
   iface = _clementine_iface()
-  if not iface :
+  if not iface:
     # Clementine not running; maybe some other media player
     print("clementine is not running?")
     return xchat.EAT_PLUGIN
-  if (len(word)) < 2 :
+  if (len(word)) < 2:
     goto = clem_info
   else:
     goto = PARAMS.get(word[1], clem_help)
   goto(iface)
   return xchat.EAT_PLUGIN
-xchat.hook_command('clem', clem , help='/clem help for what you can do')
+xchat.hook_command('clem', clem, help='/clem help for what you can do')
+
 
 def nowplaying(word, word_eol, userdata):
   " dispatcher that plays well with other xchat '/np' plugins "
   iface = _clementine_iface()
   del(word, word_eol, userdata)
-  if iface :
+  if iface:
     clem_np(iface)
-  return xchat.EAT_NONE # let another plugin pick it up
+  return xchat.EAT_NONE  # let another plugin pick it up
 xchat.hook_command('np', nowplaying, help='same as /clem np')
 
 print("\x02%s\x02 (%s) %s" % (__module_name__, __module_version__, __module_description__))
--- mocp_xchat.py
+++ mocp_xchat.py
@@ -5,102 +5,114 @@ from __future__ import print_function
 import xchat
 import commands
 
-__module_name__  = "MOCP_control"
-__module_version__  = "20130620"
-__module_description__  = "control music from inside xchat"
-__module_author__  =  "Mozai, kubicz10"
+__module_name__ = "MOCP_control"
+__module_version__ = "20130620"
+__module_description__ = "control music from inside xchat"
+__module_author__ = "Mozai, kubicz10"
+
 
 def _mocp_state():
   " returns dict() of mocp state info, or None if not running "
   infodump = commands.getoutput("mocp --info")
-  if(infodump.find('FATAL_ERROR: The server is not running!') > -1):
+  if infodump.find('FATAL_ERROR: The server is not running!') > -1:
     # moc isn't running
     return None
-  mocstate = { }
+  mocstate = {}
   for line in str(infodump).split("\n"):
     fields = line.split(': ', 2)
     if fields[1].strip():
       mocstate[fields[0]] = fields[1].strip()
-  if not mocstate :
+  if not mocstate:
     raise Exception("mocp --info could not be parsed")
-  mocstate.setdefault('State','UNKNOWN') # just in case
+  mocstate.setdefault('State', 'UNKNOWN')  # just in case
   return mocstate
 
+
 def mocp_play(mocstate):
-  if (mocstate['State'] == "PLAY"):
+  if mocstate['State'] == "PLAY":
     print ("mocp is already playing")
   else:
     print("mocp starts playing")
     commands.getoutput("mocp --play")
 
+
 def mocp_stop(mocstate):
-  if (mocstate['State'] == "STOP"):
+  if mocstate['State'] == "STOP":
     print ("mocp isn't playing")
   else:
     print("stopping mocp")
     commands.getoutput("mocp --stop")
 
+
 def mocp_pause(mocstate):
   # using toggle-pause because it's more likely what's expected
-  if (mocstate['State'] == "PLAY"):
+  if mocstate['State'] == "PLAY":
     print("pausing mocp")
-    commands.getoutput("mocp --toggle-pause")    
-  elif (mocstate['State'] == 'PAUSE'):
+    commands.getoutput("mocp --toggle-pause")
+  elif mocstate['State'] == 'PAUSE':
     print("resuming mocp")
     commands.getoutput("mocp --toggle-pause")
   else:
     print("mocp starts playing again")
     commands.getoutput("mocp --play")
 
+
 def mocp_next(mocstate):
   del(mocstate)
   commands.getoutput("mocp --next")
 
+
 def mocp_prev(mocstate):
   del(mocstate)
   commands.getoutput("mocp --prev")
+
 
 def mocp_quit(mocstate):
   del(mocstate)
   print("killing moc")
   commands.getoutput("moc --exit")
 
+
 def mocp_info(mocstate):
-  mks = [ i for i in mocstate.keys() if i ]
+  mks = [i for i in mocstate.keys() if i]
   mks.sort()
   blurb = ''
   for i in mks:
     blurb += "\x02%s\x02 %s " % (i, mocstate[i])
   print(blurb.encode('utf-8'))
 
+
 def mocp_help(mocstate):
   del(mocstate)
-  print("\x02/mocp\x02 ["+", ".join(PARAMS.keys())+"]")
-  
+  print("\x02/mocp\x02 [" + ", ".join(PARAMS.keys()) + "]")
+
+
 def mocp_np(mocstate):
-  if (mocstate['State'] == "STOP"):
+  if mocstate['State'] == "STOP":
     # could be not running; maybe you're using a different music player
     return xchat.EAT_NONE
   blurb = [mocstate[i] for i in ('Artist', 'SongTitle') if mocstate.get(i)]
-  if blurb :
+  if blurb:
     blurb = " - ".join(blurb)
     xchat.command("me is listening to \x02%s\x02" % blurb.encode('utf-8'))
   elif mocstate.get('File'):
-    blurb = mocstate['File'][mocstate['File'].rfind('/')+1:]
+    blurb = mocstate['File'][mocstate['File'].rfind('/') + 1:]
     xchat.command("me is playing \x02%s\x02" % blurb.encode('utf-8'))
   else:
-    print("?? mocp info seems empty (%s)" % repr(mocstate.encode('utf-8'))) 
+    print("?? mocp info seems empty (%s)" % repr(mocstate.encode('utf-8')))
 
-PARAMS = { 'help':  mocp_help,
-           'np':    mocp_np,
-           'play':  mocp_play,
-           'stop':  mocp_stop,
-           'pause': mocp_pause,
-           'next':  mocp_next,
-           'prev':  mocp_prev,
-           'info':  mocp_info,
-           'quit':  mocp_quit,
-        }
+
+PARAMS = {'help': mocp_help,
+          'np': mocp_np,
+          'play': mocp_play,
+          'stop': mocp_stop,
+          'pause': mocp_pause,
+          'next': mocp_next,
+          'prev': mocp_prev,
+          'info': mocp_info,
+          'quit': mocp_quit,
+          }
+
 
 def mocp(word, word_eol, userdata):
   " dispatcher for the commands in PARAMS dict() "
@@ -109,25 +121,26 @@ def mocp(word, word_eol, userdata):
   if not mocstate:
     print("MOC is not running?")
     return xchat.EAT_NONE
-  if (len(word)) < 2 :
+  if (len(word)) < 2:
     goto = mocp_info
   else:
     goto = PARAMS.get(word[1], mocp_help)
   goto(mocstate)
   return xchat.EAT_PLUGIN
-xchat.hook_command('mocp', mocp , help='/mocp help for what you can do')
+xchat.hook_command('mocp', mocp, help='/mocp help for what you can do')
+
 
 def nowplaying(word, word_eol, userdata):
   " dispatcher that plays well with other xchat '/np' plugins "
   del(word, word_eol, userdata)
   mocstate = _mocp_state()
-  if mocstate :
+  if mocstate:
     mocp_np(mocstate)
-  return xchat.EAT_NONE # let another plugin pick it up
-xchat.hook_command('np', nowplaying, help='same as /mocp np')  
+  return xchat.EAT_NONE  # let another plugin pick it up
+xchat.hook_command('np', nowplaying, help='same as /mocp np')
 
 
 print("\x02%s\x02 (%s) %s" % (__module_name__, __module_version__, __module_description__))
 mocp_help(None)
 print("\x02/np\x02 : emotes to current channel what you're listening to")
-commands.getoutput("mocp --server") # starts it if necessary
+commands.getoutput("mocp --server")  # starts it if necessary
