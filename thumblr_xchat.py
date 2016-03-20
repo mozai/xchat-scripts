d2197858bd447bbd51f587b94575ec8252027c88
Moses Moore <moses-github@mozai.com>
2016-03-19 22:55:05 -0400

    now with threading

--- thumblr_xchat.py
+++ thumblr_xchat.py
@@ -1,26 +1,32 @@
 """
   this is a python script for use with xchat v2.x.
+  I had to change a certain name to tu\x6D\x62lr
+  because of some anti-spam that auto-blocks any urls with that name
 """
 
 # -- init
 __module_name__ = "thumblr"
-__module_version__ = "20141121"
+__module_version__ = "20150319"
 __module_description__ = "image url burp"
 __module_author__ = "Mozai <moc.iazom@sesom>"
 import xchat
-import re, time, urllib
+import re, threading, time, urllib
 from bs4 import BeautifulSoup
 
 # -- config
 # timeout between command responses
 # bots shouldn't spam, even when asked to
-COOLDOWN = 120
+COOLDOWN = 60
+CHANNELS = ['#farts', '#wetfish', '#test', '#mbtest']
+TRIGGER = '!thumblr'
 THUMBLR_URL = 'http://www.tu\x6D\x62lr.com/tagged/%s'
 USER_AGENT = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36'
 DEFAULT_TAG = 'homestuck'
 # -- end config
 
 LASTTIME = dict()
+for i in CHANNELS:
+  LASTTIME[i] = 0
 
 
 class DontBeRacist(urllib.FancyURLopener):
@@ -42,8 +48,9 @@ def fetch_posts(tag, count=16):
   """
   tag = re.sub(r'[^a-z0-9]+', '-', tag.lower())
   tag = re.sub(r'^\-|\-$', '', tag)
-  response = urllib.open(THUMBLR_URL % tag)
-  soup = BeautifulSoup(response.read())
+  url_to_use = THUMBLR_URL % tag
+  response = urllib.open(url_to_use)
+  soup = BeautifulSoup(response.read(), 'html.parser')
   posts_found = []
   for node1 in soup.find_all('li', 'post'):
     post = {'href': None, 'photo_stage_img': None, 'tags': [], 'notes': 0}
@@ -71,7 +78,7 @@ def fetch_posts(tag, count=16):
 
 
 def thumblrCommand(word, word_eol, userdata):
-  """ respond to "/hsg (command)" messages
+  """ respond to "/thumblr (command)" messages
       prints to local client window
   """
   del(word_eol, userdata)  # shut up, pylint
@@ -88,35 +95,47 @@ def thumblrCommand(word, word_eol, userdata):
 xchat.hook_command('thumblr', thumblrCommand, help='/thumblr tagname - gives URL')
 
 
+def _search_and_emit(context, tagname):
+  " (thread) search for 'phrase' images, emit result to xchat context "
+  foundposts = fetch_posts(tagname, 1)
+  blurb1 = "\x02\x0302tu\x6Dblr\x0F {}"
+  if foundposts:
+    blurb2 = foundposts[0]['photo_stage_img']
+  else:
+    blurb2 = "(didn\'t find anything for {}".format(tagname)
+  context.command("say " + blurb1.format(blurb2))
+  return None
+
+
 def checkPrint(word, word_eol, userdata):
-  " if someone says '.hsg', respond like '/hsg co' "
+  " if someone says '!thumblr somephrase', respond like '/thumblr_say somephrase' "
   del(word_eol, userdata)  # shut up, pylint
   now = int(time.time())
   context = xchat.get_context()
-  #chan = context.get_info('channel')
-  #if ((len(channels) > 0) and (chan not in channels)):
-  #  return None
-  if not word[1].startswith('!thumblr '):
+  chan = context.get_info('channel')
+  if ((len(CHANNELS) > 0) and (chan not in CHANNELS)):
+    return None
+  words = word[1].split()
+  if words[0] != TRIGGER or len(words) < 2:
     return
-  cmd, tagname = word[1].split(' ', 1)
-  if ' ' in tagname:
-    tagname = tagname[:tagname.index(' ')]
-  tagname = tagname.lstrip('#')
+  tagname = words[1].lstrip('#')
   if not tagname:
     return
-  if LASTTIME[cmd] + COOLDOWN >= now:
-    context.command('msg %s %s timeout is %d seconds' % (word[0], cmd, COOLDOWN))
+  if LASTTIME[chan] + COOLDOWN >= now:
+    # tell requester to wait
+    context.command('msg %s %s timeout is %d seconds' % (word[0], TRIGGER, COOLDOWN))
     return xchat.EAT_PLUGIN
-  foundposts = fetch_posts(tagname, 1)
-  if foundposts:
-    blurb = "\x02\x0302tu\x6Dblr\x0F %s" % foundposts[0]['photo_stage_img']
-    context.command("say " + blurb)
-    LASTTIME[cmd] = now
-  else:
-    context.command('msg %s %s didn\'t find anything for %s' % (word[0], cmd, tagname))
+  # I'm doing something a little thread unsafe here, because I'm trusting
+  # the 'LASTTIME + COOLDOWN' above to keep too many threads
+  # from running at once
+  drone = threading.Thread(target=_search_and_emit, args=(context, tagname))
+  drone.daemon = True  # allow parent to abandon this child
+  drone.start()  # go do your thing while I go away
+  LASTTIME[chan] = now
   return xchat.EAT_PLUGIN
 
-LASTTIME['!thumblr'] = 0
+
+LASTTIME[TRIGGER] = 0
 xchat.hook_print('Channel Message', checkPrint)
 xchat.hook_print('Your Message', checkPrint)
 
@@ -124,4 +143,4 @@ xchat.hook_print('Your Message', checkPrint)
 # -- main
 print "\x02Loaded %s v%s\x02" % (__module_name__, __module_version__)
 print "\x02commands:\x02 /thumblr tagname"
-print "\x02listens:\x02 !thumblr tagname"
+print "\x02listens:\x02 {} tagname".format(TRIGGER)
