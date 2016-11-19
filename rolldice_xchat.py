ca317b1e37b20b3e17634320b06f61bddd219bac
Moses Moore <moses-github@mozai.com>
2016-11-19 16:31:22 -0500

    Python 2 to 3, xchat to hexchat

--- rolldice_xchat.py
+++ rolldice_xchat.py
@@ -1,8 +1,11 @@
 """
-  this is a python script for use with xchat v2.x.
+  this is a python script for use with hexchat
   yeah yeah everyone has a dice-rolling thing
 """
-import random, re, time, xchat
+import random
+import re
+import time
+import hexchat
 __module_name__ = "dice"
 __module_version__ = "20160412"
 __module_description__ = "roll the bones."
@@ -31,7 +34,7 @@ def _roll(what):
       if match.group(3):
         eachroll.append(int(match.group(3)))
       sumroll = sum([j for j in eachroll])
-      results.append(','.join([str(j) for j in eachroll]) + '=' + str(sumroll))
+      results.append(','.join([str(j) for j in eachroll]) + ' = ' + str(sumroll))
       continue
     match = re.search(r'^(\d*)dF$', i)
     if match:
@@ -55,37 +58,40 @@ def _roll(what):
 def checkCommand(word, word_eol, userdata):
   del(word_eol, userdata)  # shut up, pylint
   if word[1:]:
-    print "rolled dice: " + _roll(word[1:])
+    print("rolled dice: " + _roll(word[1:]))
   else:
-    print "rolled dice: " + _roll(['2d6'])
-  return xchat.EAT_PLUGIN
-xchat.hook_command('roll', checkCommand, help='roll the bones, XdY+Z')
-xchat.hook_command('dice', checkCommand, help='roll the bones, XdY+Z')
+    print("rolled dice: " + _roll(['2d6']))
+  return hexchat.EAT_PLUGIN
+hexchat.hook_command('roll', checkCommand, help='roll the bones, XdY+Z')
+hexchat.hook_command('dice', checkCommand, help='roll the bones, XdY+Z')
 
 
 def checkPrint(word, word_eol, userdata):
   del(word_eol, userdata)  # shut up pylint
   now = int(time.time())
-  context = xchat.get_context()
+  context = hexchat.get_context()
   chan = context.get_info('channel')
   if chan not in TIMEOUTS:
-    return xchat.EAT_NONE
+    return hexchat.EAT_NONE
   # who = word[0]
-  words = word[1].split()
-  print repr(words[0]) 
+  if len(word) < 2:
+    return None
+  words = word[1].strip().split()
+  if len(words) < 1:
+    # degenerate case of "nick PRIVMSG #channel :\r\n"
+    return None
   if words[0] in ('.dice', '.roll'):
-    print 'inside'
     if now <= TIMEOUTS[chan]:
-      return xchat.EAT_NONE
+      return hexchat.EAT_NONE
     if len(words) < 2:
       words.append('2d6')
-    print repr(words) 
+    print(repr(words))
     result = _roll(words[1:])
     if result:
       TIMEOUTS[chan] = now + TIMEOUT
       context.command("me rolls " + result)
-      return xchat.EAT_PLUGIN
-xchat.hook_print('Channel Message', checkPrint)
-xchat.hook_print('Your Message', checkPrint)
+      return hexchat.EAT_PLUGIN
+hexchat.hook_print('Channel Message', checkPrint)
+hexchat.hook_print('Your Message', checkPrint)
 
-print "dice loaded (/roll, .roll)"
+print("{} loaded (/roll, .roll)".format(__module_name__))
--- thumblr_xchat.py
+++ thumblr_xchat.py
@@ -1,23 +1,23 @@
 """
-  this is a python script for use with xchat v2.x.
+  this is a python script for use with hexchat
   I had to change a certain name to tu\x6D\x62lr
-  because of some anti-spam that auto-blocks any urls with that name
+  because of github thinks this file is spam if it mentions the name
 """
+# Python 3.x
+import hexchat, re, threading, time, urllib.request
+from bs4 import BeautifulSoup
 
 # -- init
 __module_name__ = "thumblr"
 __module_version__ = "20150319"
 __module_description__ = "image url burp"
 __module_author__ = "Mozai <moc.iazom@sesom>"
-import xchat
-import re, threading, time, urllib
-from bs4 import BeautifulSoup
 
 # -- config
 # timeout between command responses
 # bots shouldn't spam, even when asked to
 COOLDOWN = 60
-CHANNELS = ['#farts', '#wetfish', '#test', '#mbtest']
+CHANNELS = ['#farts', '#wetfish', '#test', ]
 TRIGGER = '!thumblr'
 THUMBLR_URL = 'http://www.tu\x6D\x62lr.com/tagged/%s'
 USER_AGENT = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36'
@@ -29,12 +29,9 @@ for i in CHANNELS:
   LASTTIME[i] = 0
 
 
-class DontBeRacist(urllib.FancyURLopener):
-  " to avoid discrimination against bots "
-  version = USER_AGENT
-
-urllib.open = DontBeRacist().open
-urllib.urlretrieve = DontBeRacist().retrieve
+# tu\x6D\x62lr is bigoted about bots
+URLOPENER = urllib.request.build_opener()
+URLOPENER.addheaders = [('User-agent', USER_AGENT)]
 
 
 def _force_to_int(text):
@@ -49,7 +46,7 @@ def fetch_posts(tag, count=16):
   tag = re.sub(r'[^a-z0-9]+', '-', tag.lower())
   tag = re.sub(r'^\-|\-$', '', tag)
   url_to_use = THUMBLR_URL % tag
-  response = urllib.open(url_to_use)
+  response = URLOPENER.open(url_to_use)
   soup = BeautifulSoup(response.read(), 'html.parser')
   posts_found = []
   for node1 in soup.find_all('li', 'post'):
@@ -83,20 +80,20 @@ def thumblrCommand(word, word_eol, userdata):
   """
   del(word_eol, userdata)  # shut up, pylint
   if len(word) == 1:
-    print "/thumblr tagname ie.: '/thumblr cosplay'"
+    print("/thumblr tagname ie.: '/thumblr cosplay'")
   elif len(word) == 2:
     foundposts = fetch_posts(word[1], 1)
     if foundposts:
-      blurb = "Have a look at %s" % foundposts[0]['photo_stage_img']
-      print blurb
-  return xchat.EAT_ALL
+      blurb = "Have a look at %s".format(foundposts[0]['photo_stage_img'])
+      print(blurb)
+  return hexchat.EAT_ALL
 
 
-xchat.hook_command('thumblr', thumblrCommand, help='/thumblr tagname - gives URL')
+hexchat.hook_command('thumblr', thumblrCommand, help='/thumblr tagname - gives URL')
 
 
 def _search_and_emit(context, tagname):
-  " (thread) search for 'phrase' images, emit result to xchat context "
+  " (thread) search for 'phrase' images, emit result to hexchat context "
   foundposts = fetch_posts(tagname, 1)
   blurb1 = "\x02\x0302tu\x6Dblr\x0F {}"
   if foundposts:
@@ -114,7 +111,7 @@ def checkPrint(word, word_eol, userdata):
   " if someone says '!thumblr somephrase', respond like '/thumblr_say somephrase' "
   del(word_eol, userdata)  # shut up, pylint
   now = int(time.time())
-  context = xchat.get_context()
+  context = hexchat.get_context()
   chan = context.get_info('channel')
   if ((len(CHANNELS) > 0) and (chan not in CHANNELS)):
     return None
@@ -127,7 +124,7 @@ def checkPrint(word, word_eol, userdata):
   if LASTTIME[chan] + COOLDOWN >= now:
     # tell requester to wait
     context.command('msg %s %s timeout is %d seconds' % (word[0], TRIGGER, COOLDOWN))
-    return xchat.EAT_PLUGIN
+    return hexchat.EAT_PLUGIN
   # I'm doing something a little thread unsafe here, because I'm trusting
   # the 'LASTTIME + COOLDOWN' above to keep too many threads
   # from running at once
@@ -135,15 +132,15 @@ def checkPrint(word, word_eol, userdata):
   drone.daemon = True  # allow parent to abandon this child
   drone.start()  # go do your thing while I go away
   LASTTIME[chan] = now
-  return xchat.EAT_PLUGIN
+  return hexchat.EAT_PLUGIN
 
 
 LASTTIME[TRIGGER] = 0
-xchat.hook_print('Channel Message', checkPrint)
-xchat.hook_print('Your Message', checkPrint)
+hexchat.hook_print('Channel Message', checkPrint)
+hexchat.hook_print('Your Message', checkPrint)
 
 
 # -- main
-print "\x02Loaded %s v%s\x02" % (__module_name__, __module_version__)
-print "\x02commands:\x02 /thumblr tagname"
-print "\x02listens:\x02 {} tagname".format(TRIGGER)
+print("\x02Loaded {} v{}\x02".format(__module_name__, __module_version__))
+print("\x02commands:\x02 /thumblr tagname")
+print("\x02listens:\x02 {} tagname".format(TRIGGER))
